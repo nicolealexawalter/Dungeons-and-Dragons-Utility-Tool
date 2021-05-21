946ce9c8f7b387616874deaec5b395f7ec171158
Nicole <83924569+nicolealexawalter@users.noreply.github.com>
2021-05-21 18:40:08 -0400

    Create resize.js

---
+++ js/resize.js
@@ -0,0 +1,12 @@
+function resize_canvas(){
+            canvas = document.getElementById("canvas");
+            if (canvas.width  < window.innerWidth)
+            {
+                canvas.width  = window.innerWidth;
+            }
+
+            if (canvas.height < window.innerHeight)
+            {
+                canvas.height = window.innerHeight;
+            }
+        }
